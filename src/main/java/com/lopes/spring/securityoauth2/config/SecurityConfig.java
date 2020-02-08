16b61aa07d574d56312754d436cf7a1d273b8964
Anderson Lopes <anderson.lopes@gmail.com>
2020-02-08 20:38:39 +0000

    Define users of access.

--- src/main/java/com/lopes/spring/securityoauth2/config/SecurityConfig.java
+++ src/main/java/com/lopes/spring/securityoauth2/config/SecurityConfig.java
@@ -1,11 +1,34 @@
 package com.lopes.spring.securityoauth2.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //User to access application
+        auth.inMemoryAuthentication().withUser("lopes").password("lopes").roles("ADMIN");
+    }
 }
