8290fe1668813689c14fe5afb5f21f414a750018
Anderson Lopes <anderson.lopes@gmail.com>
2020-02-08 18:57:03 +0000

    First commit.

---
+++ src/test/java/com/lopes/spring/securityoauth2/SecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.spring.securityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
